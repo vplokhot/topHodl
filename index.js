db1fb71c05bcc1b3d7020a6a14668a1bcbfd401a
Valentin Plokhotniouk <vplokhot@Valentins-MacBook-Pro.local>
2020-11-09 09:22:43 -0800

    dotenv added

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const Web3 = require("web3")
-
+require('dotenv').config();
 const INFURA_KEY = process.env.INFURA_KEY;
 const web3 = new Web3(new Web3.providers.HttpProvider(INFURA_KEY))
-
+console.log(INFURA_KEY, "key")
 web3.eth.getBalance("0xa7DE087329BFcda5639247F96140f9DAbe3DeED1", function(err, result) {
   if (err) {
     console.log(err)
